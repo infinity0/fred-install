f2425d1c8f58557ab0d8243330565b1da9865898
Ximin Luo <infinity0@gmx.com>
2010-11-14 18:19:30 +0000

    move everything except input files into freenet/ and use sh.script.in directly from wrapper source

--- build.run.sh
+++ build.run.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-cp sh.script.in run.sh
+SRC="../contrib/wrapper/src/bin/sh.script.in"
+DST="freenet/run.sh"
+
+cp "$SRC" "$DST" || exit 1
 patch -p0 < run.sh.diff
 sed -i \
   -e 's/@app.name@/Freenet/g' \
   -e 's/@app.long.name@/Freenet 0.7 (experimental release)/g' \
   -e 's/@app.description@/Freenet reference daemon/g' \
-  run.sh
-chmod +x run.sh
+  "$DST"
+chmod +x "$DST"
 
